e9c94208542940ceb031e67f02d9185fdee90575
Jakob Schweter <kalp@Jakobs-MacBook-Pro.local>
2023-07-31 19:02:21 -0500

    basic setup for sizing at different screen sizes

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -3,22 +3,26 @@ import ProjectCard from "./components/ProjectCard";
 function App() {
   return (
     <>
-      <div>Hello!</div>
-      <ProjectCard
-        title="Tune Transfer"
-        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
-        background="bg-blue-100"
-      />
-      <ProjectCard
-        title="Wealth Wizard"
-        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
-        background="bg-emerald-100"
-      />
-      <ProjectCard
-        title="Wealth Wizard"
-        body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
-        background="bg-purple-100"
-      />
+      <div className="grid grid-cols-1 place-items-center w-fit h-fit md:grid-cols-2 lg:grid-cols-2">
+        <ProjectCard
+          title="Tune Transfer"
+          body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
+          background="bg-blue-100"
+          image="https://cdnb.artstation.com/p/assets/covers/images/012/730/781/large/sean-hicks-capture.jpg?1536225811"
+        />
+        <ProjectCard
+          title="Wealth Wizard"
+          body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
+          background="bg-emerald-100"
+          image="https://www.giantbomb.com/a/uploads/scale_medium/3/34651/3394061-stupidbird.png"
+        />
+        <ProjectCard
+          title="CarCar"
+          body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
+          background="bg-purple-100"
+          image="https://www.models-resource.com/resources/big_icons/15/14951.png?updated=1460924485"
+        />
+      </div>
     </>
   );
 }
--- site/src/components/ProjectCard.tsx
+++ site/src/components/ProjectCard.tsx
@@ -2,25 +2,37 @@ type ProjectCardProps = {
   title: string;
   body: string;
   background: string;
+  image: string;
 };
 
 export default function ProjectCard({
   title,
   body,
   background,
+  image,
 }: ProjectCardProps) {
   const cardConfig = {
     bgColor: background,
+    image,
+  };
+
+  const handleClick = () => {
+    console.log("Clicked on project");
   };
 
   return (
-    <>
-      <div
-        className={`w-3/6 m-6 p-6 rounded-xl shadow-md ${cardConfig.bgColor}`}
-      >
-        <span className="text-2xl font-bold">{title}</span>
-        <div>{body}</div>
-      </div>
-    </>
+    <div
+      className={`w-3/4 m-3 p-6 rounded-xl shadow-md hover:cursor-pointer hover:scale-105 ${cardConfig.bgColor}`}
+      onClick={handleClick}
+    >
+      <a>
+        <img
+          className="mx-auto w-3/6 h-3/6 mb-3"
+          src={cardConfig.image}
+        ></img>
+      </a>
+      <span className="text-2xl font-bold">{title}</span>
+      <div>{body}</div>
+    </div>
   );
 }
