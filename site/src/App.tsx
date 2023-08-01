b745fa18e7fe9edcc08245e605390579f6fb5797
Jakob Schweter <kalp@Jakobs-MBP.lan>
2023-08-01 13:15:45 -0500

    created panel component which houses elements in a grid

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,9 +1,10 @@
 import ProjectCard from "./components/ProjectCard";
+import Panel from "./components/Panel";
 
 function App() {
   return (
     <>
-      <div className="container mx-auto grid grid-cols-1 place-items-center md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+      <Panel>
         <ProjectCard
           title="Tune Transfer"
           body="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Cum, quibusdam enim expedita praesentium aut dignissimos, facere deleniti maiores laborum eligendi delectus obcaecati, vitae ipsa eos."
@@ -22,7 +23,7 @@ function App() {
           background="bg-purple-100"
           image="https://www.models-resource.com/resources/big_icons/15/14951.png?updated=1460924485"
         />
-      </div>
+      </Panel>
     </>
   );
 }
---
+++ site/src/components/Panel.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from "react";
+
+type PanelProps = {
+  children: ReactNode;
+};
+
+export default function Panel({ children }: PanelProps) {
+  return (
+    <div className="container mx-auto grid grid-cols-1 place-items-center h-screen md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+      {children}
+    </div>
+  );
+}
