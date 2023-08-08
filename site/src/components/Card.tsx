3dc6428d0e2b268101d5b220e4fa38a9a64a3961
Jakob Schweter <kalp@Jakobs-MBP.lan>
2023-08-07 22:57:52 -0500

    added a new effect to scrolling for porjects, might vhange

--- site/src/components/Card.tsx
+++ site/src/components/Card.tsx
@@ -5,11 +5,17 @@ type CardProps = {
   image: string;
   title: string;
   body: string;
-  date: string;
   icons: ReactNode[];
+  link: string;
 };
 
-export default function Card({ image, title, body, icons }: CardProps) {
+export default function Card({
+  image,
+  title,
+  body,
+  icons,
+  link,
+}: CardProps) {
   const renderedIcons = icons.map((icon, index) => {
     return (
       <span className="icon ml-2 mt-5" key={index}>
@@ -18,27 +24,32 @@ export default function Card({ image, title, body, icons }: CardProps) {
     );
   });
 
+  const handleCardClick = () => {};
+
   return (
-    <motion.div
-      className="card is-clickable"
-      whileHover={{ scale: 1.05, transition: { duration: 0.25 } }}
-    >
-      <div className="card-image">
-        <figure className="image is-4by3">
-          <img src={image} alt="Placeholder image" />
-        </figure>
-      </div>
-      <div className="card-content">
-        <div className="media">
-          <div className="media-left">
-            <p className="title is-4">{title}</p>
+    <a href={link}>
+      <motion.div
+        className="card is-clickable"
+        onClick={handleCardClick}
+        whileHover={{ scale: 1.05, transition: { duration: 0.25 } }}
+      >
+        <div className="card-image">
+          <figure className="image is-4by3">
+            <img src={image} alt="project image" />
+          </figure>
+        </div>
+        <div className="card-content">
+          <div className="media">
+            <div className="media-left">
+              <p className="title is-4">{title}</p>
+            </div>
+          </div>
+          <div className="media-content has-text-left">
+            {body}
+            <p className="has-text-right">{renderedIcons}</p>
           </div>
         </div>
-        <div className="media-content has-text-left">
-          {body}
-          <p className="has-text-right">{renderedIcons}</p>
-        </div>
-      </div>
-    </motion.div>
+      </motion.div>
+    </a>
   );
 }
--- site/src/components/Projects.tsx
+++ site/src/components/Projects.tsx
@@ -1,7 +1,6 @@
-import { motion } from "framer-motion";
+import { motion, useTransform } from "framer-motion";
 import placeholder from "../media/placeholder.png";
 import ttHome from "../media/tuneTransfer/ttHome.png";
-import ttConvert from "../media/tuneTransfer/ttConvert.png";
 import Card from "./Card";
 import {
   SiDjango,
@@ -14,26 +13,38 @@ import {
   SiMongodb,
   SiRedux,
 } from "react-icons/si";
+import { useScroll } from "framer-motion";
+import { useRef, MutableRefObject } from "react";
 
 export default function Projects() {
+  const ref = useRef() as MutableRefObject<HTMLDivElement>;
+
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["0 1", "1.33 1"],
+  });
+
+  const scaleProgress = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+  const opacityProgress = useTransform(scrollYProgress, [0, 1], [0.6, 1]);
+
   return (
-    <section className="section has-background-primary">
+    <section ref={ref} className="section has-background-primary">
       <div className="container">
         <div className="box">
           <div className="has-text-centered title">My Work</div>
           <motion.div
             className="columns is-centered has-text-centered"
             initial={{ opacity: 0 }}
-            whileInView={{ opacity: 1 }}
-            transition={{ delay: 0.3 }}
-            viewport={{ once: false }}
+            style={{
+              scale: scaleProgress,
+              opacity: opacityProgress,
+            }}
           >
             <div className="column">
               <Card
                 image={ttHome}
                 title="Tune Transfer"
                 body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat quo voluptate tempore vitae explicabo iste ipsam nihil. Nemo, vero."
-                date="July 2023"
                 icons={[
                   <SiYoutube size={30} />,
                   <SiSpotify size={30} />,
@@ -42,6 +53,7 @@ export default function Projects() {
                   <SiPython size={30} />,
                   <SiJavascript size={30} />,
                 ]}
+                link="https://gitlab.com/jako1001/tune-transfer"
               />
             </div>
             <div className="column">
@@ -49,7 +61,6 @@ export default function Projects() {
                 image={placeholder}
                 title="Wealth Wizard"
                 body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat quo voluptate tempore vitae explicabo iste ipsam nihil. Nemo, vero."
-                date="April 2023"
                 icons={[
                   <SiMongodb size={30} />,
                   <SiFastapi size={30} />,
@@ -58,6 +69,7 @@ export default function Projects() {
                   <SiPython size={30} />,
                   <SiJavascript size={30} />,
                 ]}
+                link="https://hrgroup4.gitlab.io/wealth-wizard/"
               />
             </div>
             <div className="column">
@@ -65,7 +77,6 @@ export default function Projects() {
                 image={placeholder}
                 title="CarCar"
                 body="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad fugiat quo voluptate tempore vitae explicabo iste ipsam nihil. Nemo, vero."
-                date="March 2023"
                 icons={[
                   <SiDjango size={30} />,
                   <SiReact size={30} />,
