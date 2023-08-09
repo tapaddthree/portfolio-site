e7b55bdcdbe7695662926991a6f50658e8b913c0
Jakob Schweter <kalp@Jakobs-MacBook-Pro.local>
2023-08-09 15:21:53 -0500

    rearranged skills on skills component

--- site/src/components/Skills.tsx
+++ site/src/components/Skills.tsx
@@ -23,21 +23,21 @@ export default function Skills() {
   const [skillName, setSkillName] = useState("");
 
   const skills = [
-    { icon: <SiDjango size={80} />, name: "Django" },
-    { icon: <SiSpotify size={80} />, name: "Spotify" },
-    { icon: <SiYoutube size={80} />, name: "YouTube" },
-    { icon: <SiPython size={80} />, name: "Python" },
     { icon: <SiReact size={80} />, name: "React" },
+    { icon: <SiPython size={80} />, name: "Python" },
     { icon: <SiFastapi size={80} />, name: "FastAPI" },
     { icon: <SiJavascript size={80} />, name: "JavaScript" },
     { icon: <SiTypescript size={80} />, name: "TypeScript" },
+    { icon: <SiVite size={80} />, name: "Vite" },
+    { icon: <SiBulma size={80} />, name: "Bulma" },
+    { icon: <SiDocker size={80} />, name: "Docker" },
+    { icon: <SiDjango size={80} />, name: "Django" },
     { icon: <SiMongodb size={80} />, name: "MongoDB" },
     { icon: <SiRedux size={80} />, name: "Redux" },
     { icon: <SiGithub size={80} />, name: "Github" },
     { icon: <SiGitlab size={80} />, name: "Gitlab" },
-    { icon: <SiVite size={80} />, name: "Vite" },
-    { icon: <SiBulma size={80} />, name: "Bulma" },
-    { icon: <SiDocker size={80} />, name: "Docker" },
+    { icon: <SiSpotify size={80} />, name: "Spotify" },
+    { icon: <SiYoutube size={80} />, name: "YouTube" },
   ];
 
   const renderedSkills = skills.map((skill, index) => {
