260ce0c73ee963cd79ff00ec8f4897ff3bda9b3a
Jakob Schweter <kalp@Jakobs-MBP.lan>
2023-08-11 15:57:49 -0500

    change some styling

--- site/src/components/FooterFormSection.tsx
+++ site/src/components/FooterFormSection.tsx
@@ -4,7 +4,7 @@ export default function FooterFormSection() {
   return (
     <section className="section has-text-black">
       <div className="container is-max-widescreen">
-        <div className="has-text-centered is-size-3 mb-6 is-italic has-text-weight-semibold">
+        <div className="has-text-centered is-size-3 mb-6 is-italic has-text-weight-semibold has-text-black">
           Let's talk.
         </div>
         <ContactForm />
--- site/src/components/Projects.tsx
+++ site/src/components/Projects.tsx
@@ -4,7 +4,9 @@ export default function ProjectsPage() {
   return (
     <section className="section has-background-primary">
       <div className="container ">
-        <div className="has-text-centered title">My Work</div>
+        <div className="has-text-black has-text-centered title">
+          My Work
+        </div>
         <div className="columns is-centered is-multiline has-text-centered">
           {projects}
         </div>
