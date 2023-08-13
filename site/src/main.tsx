f32d18a6db7df23d07c93f911d4b36f5533618e6
Jakob Schweter <kalp@Jakobs-MBP.lan>
2023-08-12 21:50:51 -0500

    add analytics

--- site/src/main.tsx
+++ site/src/main.tsx
@@ -1,6 +1,8 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-// import "../node_modules/bulma/css/bulma.min.css";
+import { inject } from "@vercel/analytics";
 import "../sass/mystyles.css";
+
+inject();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
